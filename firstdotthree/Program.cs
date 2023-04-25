c314a5aaf8b375cc0a1c652094aeab9c1ee78234
Компьютер <@DESKTOP-0RODP2R>
2023-04-25 22:17:51 +0200

    Добавьте файлы проекта.

---
+++ firstdotthree/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        double sum = 0;
+        for (int i = 0; i < 12; i ++)
+        {
+            sum += ball_volume(10 + i * 0.5);
+        }
+        double ball_volume(double d)
+        {
+            return (4 * Math.PI / 3) * d * d * d;
+        }
+        Console.WriteLine(sum);
+    }
+}
